693f58dbb56674d80ac4cfe0de44a99a18030f1c
Arukenofu <balkenov711@gmail.com>
2024-09-02 11:38:44 +0500

    updated most packages to latest version, improved some styles, types, logic. added useResponsive composable for responsiveness for mobile devices

--- src/shared/composables/useResponsive.ts
+++ src/shared/composables/useResponsive.ts
@@ -22,6 +22,26 @@ export default function () {
     return {
         isMobile,
         screenWidth,
-        screenHeight
+        screenHeight,
+        getOs
+    }
+}
+
+
+function getOs() {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+
+    if (userAgent.indexOf('android') !== -1) {
+        return 'Android';
+    } else if (userAgent.indexOf('iphone') !== -1 || userAgent.indexOf('ipad') !== -1) {
+        return 'iOS';
+    } else if (userAgent.indexOf('win') !== -1) {
+        return 'Windows';
+    } else if (userAgent.indexOf('mac') !== -1) {
+        return 'macOS';
+    } else if (userAgent.indexOf('linux') !== -1) {
+        return 'Linux';
+    } else {
+        return 'Unknown';
     }
 }
