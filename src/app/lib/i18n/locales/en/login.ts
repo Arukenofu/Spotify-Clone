5c5d30db71410127fccdc5efccadb1037fd01b8d
Arukenofu <balkenov711@gmail.com>
2024-12-25 16:19:25 +0500

    added translation for /login page

---
+++ src/app/lib/i18n/locales/en/login.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'Log in to Spotify',
+    login: 'Log In',
+
+    emailOrUsername: 'Email or username',
+    password: 'Password',
+
+    rememberMe: 'Remember me',
+    noAccount: 'Don\'t have an account?',
+    noAccountLink: 'Sign up for Spotify'
+}
---
+++ src/app/lib/i18n/locales/ru/login.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'Войти в Spotify',
+    login: 'Войти',
+
+    emailOrUsername: 'Электронная почта или имя пользователя',
+    password: 'Пароль',
+
+    rememberMe: 'Запомнить меня',
+    noAccount: 'Нет аккаунта?',
+    noAccountLink: 'Регистрация в Spotify'
+}
